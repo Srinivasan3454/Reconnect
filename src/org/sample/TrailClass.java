22bbbcf9b3e74786b60d59290562c7c8c4f3d758
Srinivasan3454 <Srinivasansankaran25@gmail.com>
2025-06-04 09:41:08 +0530

    RE

---
+++ src/org/sample/TrailClass.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+
+
+public class TrailClass {
+	
+	
+	private void sample() {
+		int a= 25;
+		System.out.println(a);
+		
+	}
+
+	private void SampleModel() {
+		int a = 50;
+		System.out.println(a);
+		
+	} 
+public static void main(String[] args) {
+	TrailClass t = new TrailClass();
+	t.sample();
+	t.SampleModel();
+}
+
+	
+	
+	
+}
